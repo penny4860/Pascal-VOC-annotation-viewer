192957f31fe79fa6513f31949cb751ac24522f8f
jeongjoonsup <penny4860@gmail.com>
2017-03-27 23:09:16 +0900

    main package renamed

---
+++ app.py
@@ -0,0 +1,15 @@
+import sys
+from PyQt4 import QtGui, uic
+
+class MyWindow(QtGui.QMainWindow):
+    def __init__(self):
+        super(MyWindow, self).__init__()
+        uic.loadUi('image_window.ui', self)
+        self.show()
+
+if __name__ == '__main__':
+    app = QtGui.QApplication(sys.argv)
+    window = MyWindow()
+    sys.exit(app.exec_())
+    
+    
