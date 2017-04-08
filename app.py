506a4cdeae5554eeb615cb6969841ae02904a9d0
joonsup <joonsup@joonsup-PC>
2017-04-08 14:16:07 +0900

    temp : pyqt5 test

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 import sys
-from PyQt4 import QtGui, uic
 
-class MyWindow(QtGui.QMainWindow):
+from PyQt5 import QtGui, uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class MyWindow(QMainWindow):
     def __init__(self):
         super(MyWindow, self).__init__()
         uic.loadUi('image_window.ui', self)
         self.show()
 
 if __name__ == '__main__':
-    app = QtGui.QApplication(sys.argv)
+    app = QApplication(sys.argv)
     window = MyWindow()
     sys.exit(app.exec_())
     
