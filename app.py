77412a6f274d2adedd72400e27c0ef4b2b6ebbf5
joonsup <joonsup@joonsup-PC>
2017-04-08 16:00:08 +0900

    image files 로드 성공

--- app.py
+++ app.py
@@ -4,21 +4,42 @@ import sys
 import cv2
 
 from PyQt5 import QtGui, uic
-from PyQt5.QtWidgets import QDialog, QApplication, QPushButton, QVBoxLayout, QMainWindow, QWidget
+from PyQt5.QtWidgets import QDialog, QApplication, QPushButton, QVBoxLayout, QMainWindow, QWidget, QFileDialog
 
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 import matplotlib.pyplot as plt
 
 
+class Model:
+    """
+    # Attributes
+        image_files : list of strings
+    
+    """
+    def __init__(self, viewer):
+        self._viewer = viewer
+        self.image_files = []
+
+    def change(self, image_files=None):
+        if image_files:
+            self.image_files = image_files
+
+        self.notify_viewer()
+
+    def notify_viewer(self):
+        self._viewer.update()
+        
+
 class MyWindow(QMainWindow):
     def __init__(self):
         super(MyWindow, self).__init__()
         uic.loadUi('image_window.ui', self)
-        self.show()
+        self.model = Model(self)
         
+        self.show()
         self.init_ui()
-        self.plot()
+        # self.plot()
         
     def init_ui(self):
         # a figure instance to plot on
@@ -36,26 +57,31 @@ class MyWindow(QMainWindow):
         self.display_layout.addWidget(self.toolbar)
         self.display_layout.addWidget(self.canvas)
         
-    def plot(self):
-        ''' plot some random stuff '''
+        self.actionLoad_images.triggered.connect(self._open_file_dialog)
+
+    
+    def _open_file_dialog(self):
+        files, _ = QFileDialog.getOpenFileNames(self, 'Open file', "", "Image files (*.png)")
+        
+        if files:
+            self.model.change(image_files=files)
+        else:
+            pass
+            
+        
+    def update(self):
         self.figure.clear()
-
-        # create an axis
-        ax = self.figure.add_subplot(2, 2, 1)
-        image = cv2.imread("images//1.png")
-        ax.imshow(image)
-
-        ax = self.figure.add_subplot(2, 2, 2)
-        image = cv2.imread("images//2.png")
-        ax.imshow(image)
-
-        ax = self.figure.add_subplot(2, 2, 3)
-        image = cv2.imread("images//3.png")
-        ax.imshow(image)
-
-        ax = self.figure.add_subplot(2, 2, 4)
-        image = cv2.imread("images//4.png")
-        ax.imshow(image)
+        
+        n_rows = 2
+        n_cols = 2
+        
+        for i in range(4):
+            filename = self.model.image_files[i]
+            
+            if filename:
+                ax = self.figure.add_subplot(2, 2, i+1)
+                image = cv2.imread(filename)
+                ax.imshow(image)
 
         # refresh canvas
         self.canvas.draw()
