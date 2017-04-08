ac766510abf9bf783ceea4075af9b8d768b0b0ef
joonsup <joonsup@joonsup-PC>
2017-04-08 15:18:40 +0900

    main window app 에 sample image 4개를 plot 성공

--- app.py
+++ app.py
@@ -3,7 +3,11 @@
 import sys
 
 from PyQt5 import QtGui, uic
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QDialog, QApplication, QPushButton, QVBoxLayout, QMainWindow, QWidget
+
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+import matplotlib.pyplot as plt
 
 
 class MyWindow(QMainWindow):
@@ -11,6 +15,55 @@ class MyWindow(QMainWindow):
         super(MyWindow, self).__init__()
         uic.loadUi('image_window.ui', self)
         self.show()
+        
+        self.init_ui()
+        self.plot()
+        
+    def init_ui(self):
+        # a figure instance to plot on
+        self.figure = plt.figure()
+
+        # this is the Canvas Widget that displays the `figure`
+        # it takes the `figure` instance as a parameter to __init__
+        self.canvas = FigureCanvas(self.figure)
+
+        # this is the Navigation widget
+        # it takes the Canvas widget and a parent
+        self.toolbar = NavigationToolbar(self.canvas, self)
+
+        # set the layout
+        self.display_layout.addWidget(self.toolbar)
+        self.display_layout.addWidget(self.canvas)
+        
+    def plot(self):
+        ''' plot some random stuff '''
+        # random data
+        import cv2
+
+        # instead of ax.hold(False)
+        self.figure.clear()
+
+        # create an axis
+        #ax = self.figure.add_subplot(111)
+        ax = self.figure.add_subplot(2, 2, 1)
+        image = cv2.imread("images//1.png")
+        ax.imshow(image)
+
+        ax = self.figure.add_subplot(2, 2, 2)
+        image = cv2.imread("images//2.png")
+        ax.imshow(image)
+
+        ax = self.figure.add_subplot(2, 2, 3)
+        image = cv2.imread("images//3.png")
+        ax.imshow(image)
+
+        ax = self.figure.add_subplot(2, 2, 4)
+        image = cv2.imread("images//4.png")
+        ax.imshow(image)
+
+        # refresh canvas
+        self.canvas.draw()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
