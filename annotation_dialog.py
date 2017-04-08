7326738d1098990ba71c388405a1cefce0cdeba6
joonsup <joonsup@joonsup-PC>
2017-04-08 21:17:14 +0900

    nit : code 정리

--- annotation_dialog.py
+++ annotation_dialog.py
@@ -3,9 +3,9 @@
 from PyQt5.QtWidgets import QDialog, QApplication, qApp
 from PyQt5 import uic
 
-class DateDialog(QDialog):
+class AnnotationInputDialog(QDialog):
     def __init__(self, parent = None):
-        super(DateDialog, self).__init__(parent)
+        super(AnnotationInputDialog, self).__init__(parent)
         uic.loadUi('AnnotationDialog.ui', self)
         
         self.cb.addItem("SvhnAnnotation")
@@ -13,17 +13,14 @@ class DateDialog(QDialog):
         
         self.btn.clicked.connect(qApp.quit)
 
-    def get_annotation(self):
-        return self.cb.currentText()
-
     @staticmethod
     def getAnnotation(parent = None):
-        dialog = DateDialog(parent)
+        dialog = AnnotationInputDialog(parent)
         _ = dialog.exec_()
         ann = dialog.cb.currentText()
         return ann
 
 if __name__ == '__main__':
     app = QApplication([])
-    ann = DateDialog.getAnnotation()
+    ann = AnnotationInputDialog.getAnnotation()
     print ann
